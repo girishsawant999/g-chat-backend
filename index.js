9a5c98257d42526a9efaf8652db899925574ebad
Girish <girishsawant999.gs@gmail.com>
2020-07-26 00:58:39 -0700

    Add timing for messages

---
+++ index.js
@@ -0,0 +1,31 @@
+// Node server which will handle request
+const io = require("socket.io")(process.env.PORT || 8000);
+const time = new Date();
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (user) => {
+    users[socket.id] = user;
+    socket.broadcast.emit("user-joined", socket.id, users);
+    socket.emit("get-users-list", socket.id, users);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      time:  time.toLocaleString('en-IN', { hour: 'numeric', minute: 'numeric', hour12: true }),
+      user: users[socket.id],
+    });
+  });
+
+  socket.on("typing", (isTyping) => {
+    socket.broadcast.emit("typing-Waiting", {
+      user: isTyping ? users[socket.id] : null,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("left", socket.id, users);
+    delete users[socket.id];
+  });
+});
